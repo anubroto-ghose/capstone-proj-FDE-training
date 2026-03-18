c51adb83b9233503604fe8e36c7b10b7a2637a1f
anubroto-ghose <anubroto.g@prodapt.com>
2026-03-18 10:40:13 +0530

    Moved Stuff Outside of source

--- setup/milvus_check.py
+++
@@ -1,7 +0,0 @@
-from pymilvus import MilvusClient
-
-client = MilvusClient("http://localhost:19530")
-
-# List collections
-collections = client.list_collections()
-print("Collections:", collections)
--- setup/postgres_check.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING", "")
-conn = psycopg2.connect(CONNECTION_STRING)
-print("Connected to Postgres!")
